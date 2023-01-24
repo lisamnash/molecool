d3e3a56d344681f0693b240ce7f757ea150bf518
Lisa Nash <lisa.michelle.nash@gmail.com>
2023-01-23 22:05:11 -0800

    move atom data to new module

--- molecool/functions.py
+++ molecool/functions.py
@@ -263,18 +263,6 @@ def calculate_distance(rA, rB):
     return distance
 
 
-atom_colors = {
-    "H": "white",
-    "C": "#D3D3D3",
-    "N": "#add8e6",
-    "O": "red",
-    "P": "#FFA500",
-    "F": "#FFFFE0",
-    "Cl": "#98FB98",
-    "Br": "#F4A460",
-    "S": "yellow",
-}
-
 
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
